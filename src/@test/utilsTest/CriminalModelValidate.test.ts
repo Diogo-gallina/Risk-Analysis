c695e15fe9c48d36bba8fa8b478854719ac6dd32
Diogo-gallina <diogogallina20@gmail.com>
2023-06-30 04:40:32 -0300

    feat: add-test-to-CriminalModelValidate

---
+++ src/@test/utilsTest/CriminalModelValidate.test.ts
@@ -0,0 +1,14 @@
+import { CriminalModelValidate } from "../../utils/CriminalModelValidate";
+
+test('Should return correct code based on provided criminal status', () => {
+    const criminalStatus = "no";
+    const score = CriminalModelValidate.criminalModelValidaTe(criminalStatus);
+    expect(score).toBe(0);
+});
+
+
+test('Should return correct code based on provided criminal status', () => {
+    const criminalStatus = "yes";
+    const score = CriminalModelValidate.criminalModelValidaTe(criminalStatus);
+    expect(score).toBe(-1);
+});
