66317c8aff51d6ae31a746d320309257b30c9dd5
Diogo-gallina <diogogallina20@gmail.com>
2023-06-30 11:26:34 -0300

    feat: add-jest FinancialResult

--- src/@test/utilsTest/FinancialResultValidate.test.ts
+++ src/@test/utilsTest/FinancialResultValidate.test.ts
@@ -1,21 +1,4 @@
-// export class FinancialResultValidate {
-//     public static calculateFinancialResult(
-//         annualRecipe: number,
-//         annualExpense: number
-//     ): 'profit' | 'prejudice' {
-//         if (annualRecipe > annualExpense) {
-//         return 'profit';
-//         } else {
-//         return 'prejudice';
-//         }
-//     }
 
-//     public static totalScoreFinancialCalculate(
-//         choiceFinancial: string
-//     ){
-//         return choiceFinancial === 'profit' ? 15 : 0; 
-//     }
-// }
 
 
 import { FinancialResultValidate } from "../../utils/FinancialResultValidate";
