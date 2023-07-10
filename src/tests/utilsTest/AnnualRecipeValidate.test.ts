03155cd545b707ff048fd36fbcda961ff9f5d783
Diogo-gallina <diogogallina20@gmail.com>
2023-07-10 09:43:54 -0300

    refactor: add-describe-in-AnnualRecipeValue tests

--- src/tests/utilsTest/AnnualRecipeValidate.test.ts
+++ src/tests/utilsTest/AnnualRecipeValidate.test.ts
@@ -1,83 +1,89 @@
 import { AnnualRecipeValidate } from '../../utils/AnnualRecipeValidate';
 
-// Positive tests with result < 300k
+describe('Annual recipe validate ', () => {
 
-test('calculateAnnualRecipeScore returns the correct score', () => {
-    const annualRecipe = 125000;
-    const score = AnnualRecipeValidate.calculateAnnualRecipeScore(annualRecipe);
-    expect(score).toBe(-1);
-});
+    describe('Positive tests with result < 300k', () => {
 
-test('calculateAnnualRecipeScore returns the correct score', () => {
-    const annualRecipe = 83201;
-    const score = AnnualRecipeValidate.calculateAnnualRecipeScore(annualRecipe);
-    expect(score).toBe(-1);
-});
+        test('Calculate annual recipe score returns the correct score', () => {
+            const annualRecipe = 125000;
+            const score = AnnualRecipeValidate.calculateAnnualRecipeScore(annualRecipe);
+            expect(score).toBe(-1);
+        });
 
-test('calculateAnnualRecipeScore returns the correct score', () => {
-    const annualRecipe = 289000;
-    const score = AnnualRecipeValidate.calculateAnnualRecipeScore(annualRecipe);
-    expect(score).toBe(-1);
-});
+        test('Calculate annual recipe score returns the correct score', () => {
+            const annualRecipe = 83201;
+            const score = AnnualRecipeValidate.calculateAnnualRecipeScore(annualRecipe);
+            expect(score).toBe(-1);
+        });
 
-// Positive tests with result 5
+        test('Calculate annual recipe score returns the correct score', () => {
+            const annualRecipe = 289000;
+            const score = AnnualRecipeValidate.calculateAnnualRecipeScore(annualRecipe);
+            expect(score).toBe(-1);
+        });
+    });    
 
-test('calculateAnnualRecipeScore returns the correct score', () => {
-    const annualRecipe = 500000;
-    const score = AnnualRecipeValidate.calculateAnnualRecipeScore(annualRecipe);
-    expect(score).toBe(5);
-});
+    describe('Positive tests with result 5', () => {
 
-test('calculateAnnualRecipeScore returns the correct score', () => {
-    const annualRecipe = 354215;
-    const score = AnnualRecipeValidate.calculateAnnualRecipeScore(annualRecipe);
-    expect(score).toBe(5);
-});
+        test('Calculate annual recipe score returns the correct score', () => {
+            const annualRecipe = 500000;
+            const score = AnnualRecipeValidate.calculateAnnualRecipeScore(annualRecipe);
+            expect(score).toBe(5);
+        });
 
-test('calculateAnnualRecipeScore returns the correct score', () => {
-    const annualRecipe = 3054215;
-    const score = AnnualRecipeValidate.calculateAnnualRecipeScore(annualRecipe);
-    expect(score).toBe(5);
-});
+        test('Calculate annual recipe score returns the correct score', () => {
+            const annualRecipe = 354215;
+            const score = AnnualRecipeValidate.calculateAnnualRecipeScore(annualRecipe);
+            expect(score).toBe(5);
+        });
 
-// Positive tests with result 15
+        test('Calculate annual recipe score returns the correct score', () => {
+            const annualRecipe = 3054215;
+            const score = AnnualRecipeValidate.calculateAnnualRecipeScore(annualRecipe);
+            expect(score).toBe(5);
+        });
+    });    
 
-test('calculateAnnualRecipeScore returns the correct score', () => {
-    const annualRecipe = 5056320;
-    const score = AnnualRecipeValidate.calculateAnnualRecipeScore(annualRecipe);
-    expect(score).toBe(15);
-});
+    describe('Positive tests with result 5', () => {
 
-test('calculateAnnualRecipeScore returns the correct score', () => {
-    const annualRecipe = 7548546;
-    const score = AnnualRecipeValidate.calculateAnnualRecipeScore(annualRecipe);
-    expect(score).toBe(15);
-});
+        test('Calculate annual recipe score returns the correct score', () => {
+            const annualRecipe = 5056320;
+            const score = AnnualRecipeValidate.calculateAnnualRecipeScore(annualRecipe);
+            expect(score).toBe(15);
+        });
 
-test('calculateAnnualRecipeScore returns the correct score', () => {
-    const annualRecipe = 11468958;
-    const score = AnnualRecipeValidate.calculateAnnualRecipeScore(annualRecipe);
-    expect(score).toBe(15);
-});
+        test('Calculate annual recipe score returns the correct score', () => {
+            const annualRecipe = 7548546;
+            const score = AnnualRecipeValidate.calculateAnnualRecipeScore(annualRecipe);
+            expect(score).toBe(15);
+        });
 
+        test('Calculate annual recipe score returns the correct score', () => {
+            const annualRecipe = 11468958;
+            const score = AnnualRecipeValidate.calculateAnnualRecipeScore(annualRecipe);
+            expect(score).toBe(15);
+        });
+    });
 
+    describe('Positive tests with result 25 ', () => {
 
-// Positive tests with result 25 
+        test('Calculate annual recipe score returns the correct score', () => {
+            const annualRecipe = 18000000;
+            const score = AnnualRecipeValidate.calculateAnnualRecipeScore(annualRecipe);
+            expect(score).toBe(25);
+        });
 
-test('calculateAnnualRecipeScore returns the correct score', () => {
-    const annualRecipe = 18000000;
-    const score = AnnualRecipeValidate.calculateAnnualRecipeScore(annualRecipe);
-    expect(score).toBe(25);
-});
+        test('Calculate annual recipe score returns the correct score', () => {
+            const annualRecipe = 20000000;
+            const score = AnnualRecipeValidate.calculateAnnualRecipeScore(annualRecipe);
+            expect(score).toBe(25);
+        });
 
-test('calculateAnnualRecipeScore returns the correct score', () => {
-    const annualRecipe = 20000000;
-    const score = AnnualRecipeValidate.calculateAnnualRecipeScore(annualRecipe);
-    expect(score).toBe(25);
-});
+        test('Calculate annual recipe score returns the correct score', () => {
+            const annualRecipe = 14000000;
+            const score = AnnualRecipeValidate.calculateAnnualRecipeScore(annualRecipe);
+            expect(score).toBe(25);
+        });
+    });    
 
-test('calculateAnnualRecipeScore returns the correct score', () => {
-    const annualRecipe = 14000000;
-    const score = AnnualRecipeValidate.calculateAnnualRecipeScore(annualRecipe);
-    expect(score).toBe(25);
 });
