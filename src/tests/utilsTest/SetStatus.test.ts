3498d323829ebcbd91a2d367663a25402ac37074
Diogo-gallina <diogogallina20@gmail.com>
2023-07-10 09:58:38 -0300

    refactor: add-describe-in-SetStatus-tests

--- src/tests/utilsTest/SetStatus.test.ts
+++ src/tests/utilsTest/SetStatus.test.ts
@@ -1,20 +1,24 @@
 import { SetStatus } from "../../utils/SetStatus";
 
-test('Must set status correctly based on final score provided', () => {
-    const finalScore = -1;
-    const score = SetStatus.setStatusByScore(finalScore);
-    expect(score).toBe('DISAPPROVED');
-});
+describe('Set status', () => {
 
-test('Must set status correctly based on final score provided', () => {
-    const finalScore = 38;
-    const score = SetStatus.setStatusByScore(finalScore);
-    expect(score).toBe('DISAPPROVED');
-});
+    test('Must set status correctly based on final score provided', () => {
+        const finalScore = -1;
+        const score = SetStatus.setStatusByScore(finalScore);
+        expect(score).toBe('DISAPPROVED');
+    });
+
+    test('Must set status correctly based on final score provided', () => {
+        const finalScore = 38;
+        const score = SetStatus.setStatusByScore(finalScore);
+        expect(score).toBe('DISAPPROVED');
+    });
 
 
-test('Must set status correctly based on final score provided', () => {
-    const finalScore = 85;
-    const score = SetStatus.setStatusByScore(finalScore);
-    expect(score).toBe('APPROVED');
+    test('Must set status correctly based on final score provided', () => {
+        const finalScore = 85;
+        const score = SetStatus.setStatusByScore(finalScore);
+        expect(score).toBe('APPROVED');
+    });
+
 });
