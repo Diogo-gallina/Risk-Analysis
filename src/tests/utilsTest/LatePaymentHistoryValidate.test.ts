94477c3a2a6648cdff1dfcd22e410794610d52cb
Diogo-gallina <diogogallina20@gmail.com>
2023-07-10 09:57:15 -0300

    refactor: add-describe-LatePaymentHistoryValidate

--- src/tests/utilsTest/LatePaymentHistoryValidate.test.ts
+++ src/tests/utilsTest/LatePaymentHistoryValidate.test.ts
@@ -1,14 +1,17 @@
 import { LatePaymentHistoryValidate } from "../../utils/LatePaymentHistoryValidate";
 
-test('Should return correct code based on late payment history provided', () => {
-    const criminalStatus = "no";
-    const score = LatePaymentHistoryValidate.latePaymentHistoryValidate(criminalStatus);
-    expect(score).toBe(0);
-});
+describe('Late payment history validate', () => {
 
+    test('Should return correct code based on late payment history provided', () => {
+        const criminalStatus = "no";
+        const score = LatePaymentHistoryValidate.latePaymentHistoryValidate(criminalStatus);
+        expect(score).toBe(0);
+    });
 
-test('Should return correct code based on late payment history provided', () => {
-    const criminalStatus = "yes";
-    const score = LatePaymentHistoryValidate.latePaymentHistoryValidate(criminalStatus);
-    expect(score).toBe(-1);
+    test('Should return correct code based on late payment history provided', () => {
+        const criminalStatus = "yes";
+        const score = LatePaymentHistoryValidate.latePaymentHistoryValidate(criminalStatus);
+        expect(score).toBe(-1);
+    });
+
 });
