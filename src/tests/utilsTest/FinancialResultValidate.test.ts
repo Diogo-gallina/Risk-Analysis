fd5e9effb260e9c1802385f8e7bcb94a5a42747d
Diogo-gallina <diogogallina20@gmail.com>
2023-07-10 09:56:14 -0300

    refactor: add-describe-FinancialResultValidate

--- src/tests/utilsTest/FinancialResultValidate.test.ts
+++ src/tests/utilsTest/FinancialResultValidate.test.ts
@@ -3,37 +3,33 @@
 
 import { FinancialResultValidate } from "../../utils/FinancialResultValidate";
 
-//Tests for calculateFinancialResult()
+describe('Financial result validate', () => {
+        
+    test('Must correctly calculate the financial result based on annual income and annual expenses', () => {
+        const annualRecipe = 550000;
+        const annualExpense = 50000;
+        const score = FinancialResultValidate.calculateFinancialResult(annualRecipe, annualExpense);
+        expect(score).toBe('profit');
+    });
 
-test('Must correctly calculate the financial result based on annual income and annual expenses', () => {
-    const annualRecipe = 550000;
-    const annualExpense = 50000;
-    const score = FinancialResultValidate.calculateFinancialResult(annualRecipe, annualExpense);
-    expect(score).toBe('profit');
-});
+    test('Must correctly calculate the financial result based on annual income and annual expenses', () => {
+        const annualRecipe = 56220;
+        const annualExpense = 62000;
+        const score = FinancialResultValidate.calculateFinancialResult(annualRecipe, annualExpense);
+        expect(score).toBe('prejudice');
+    });
 
+    test('Should return the correct financial score value based on the given choice', () => {
+        const choiceFinancial = 'profit';
+        const score = FinancialResultValidate.totalScoreFinancialCalculate(choiceFinancial);
+        expect(score).toBe(15);
+    });
 
-test('Must correctly calculate the financial result based on annual income and annual expenses', () => {
-    const annualRecipe = 56220;
-    const annualExpense = 62000;
-    const score = FinancialResultValidate.calculateFinancialResult(annualRecipe, annualExpense);
-    expect(score).toBe('prejudice');
-});
-
-
-
-//Tests for totalScoreFinancialCalculate()
-
-test('Should return the correct financial score value based on the given choice', () => {
-    const choiceFinancial = 'profit';
-    const score = FinancialResultValidate.totalScoreFinancialCalculate(choiceFinancial);
-    expect(score).toBe(15);
-});
-
-
-test('Should return the correct financial score value based on the given choice', () => {
-    const choiceFinancial = 'prejudice';
-    const score = FinancialResultValidate.totalScoreFinancialCalculate(choiceFinancial);
-    expect(score).toBe(0);
+    test('Should return the correct financial score value based on the given choice', () => {
+        const choiceFinancial = 'prejudice';
+        const score = FinancialResultValidate.totalScoreFinancialCalculate(choiceFinancial);
+        expect(score).toBe(0);
+    });
+    
 });
 
